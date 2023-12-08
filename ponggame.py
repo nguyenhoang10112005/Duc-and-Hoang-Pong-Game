153ebe2367d1451cd894159cd9ce6aef4451893e
Duc246-sys <bui_d3@denison.edu>
2023-12-07 23:11:21 -0500

    Message

--- ponggame.py
+++ ponggame.py
@@ -13,7 +13,30 @@ sc.onkeypress(paddleadown,"s")
 sc.onkeypress(paddlebup,"Up")
 sc.onkeypress(paddlebdown,"Down")
 
+#Left Paddle 
+left_paddle = turtle.Turtle()
 
+left_paddle.speed(0)
+
+left_paddle.shape("rectangle")
+
+left_paddle.shapesize(stretch_wid=6,stretch_len=2)
+
+left_paddle.color("red")
+
+left_paddle.penup()
+
+left_paddle.goto(-400,0)
+
+
+#Right Paddle
+right_paddle = turtle.Turtle()
+
+right_paddle.speed(0)
+
+right_paddle.shape("rectangle")
+
+right_paddle.shapesize(stretch_wid=6,stretch_len=2)
 
 
 
