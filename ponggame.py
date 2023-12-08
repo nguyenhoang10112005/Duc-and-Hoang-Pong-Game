c615fc84507dcf5c3845fc18f575027d72b2eb7b
Ngọc Minh Hoàng Nguyễn <ngocminhhoangnguyen@Kens-MacBook-Pro.local>
2023-12-07 23:14:22 -0500

    Hello

--- ponggame.py
+++ ponggame.py
@@ -13,6 +13,18 @@ sc.onkeypress(paddleadown,"s")
 sc.onkeypress(paddlebup,"Up")
 sc.onkeypress(paddlebdown,"Down")
 
+## create ball
+hitBall = turtle.Turtle()
+hitBall.speed(80)
+hitBall.shape("circle")
+hitBall.color("white")
+hitBall.penup()
+hitBall.goto(0,0)
+hitBall.dx = 5
+hitBall.dy = -5
+
+
+
 ## main function
 def main():
     while True:
