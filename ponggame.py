101c6731d6a086138b6d3528a0796eb4e7c1b81c
Ngọc Minh Hoàng Nguyễn <ngocminhhoangnguyen@Kens-MacBook-Pro.local>
2023-12-07 22:53:56 -0500

    Message

---
+++ ponggame.py
@@ -0,0 +1,19 @@
+import turtle
+
+## screen
+sc = turtle.Screen()
+sc.title("Pong Game")
+sc.setup(800,800)
+sc.bgcolor("black")
+
+## set up key press
+sc.listen()
+sc.onkeypress(paddleaup,"w")
+sc.onkeypress(paddleadown,"s")
+sc.onkeypress(paddlebup,"Up")
+sc.onkeypress(paddlebdown,"Down")
+
+## main function
+def main():
+    while True:
+        sc.update()
